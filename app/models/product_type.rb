cf484cc777624d51ee029059120c40b82311ba58
Andy Duss <mindovermiles262@gmail.com>
2017-09-06 13:16:13 -0700

    add button not working

---
+++ app/models/product_type.rb
@@ -0,0 +1,4 @@
+class ProductType < ApplicationRecord
+  has_many :fields, class_name: "ProductField"
+  accepts_nested_attributes_for :fields, allow_destroy: true
+end
---
+++ db/migrate/20170906195150_create_product_fields.rb
@@ -0,0 +1,11 @@
+class CreateProductFields < ActiveRecord::Migration[5.1]
+  def change
+    create_table :product_fields do |t|
+      t.string :name
+      t.string :field_type
+      t.belongs_to :product_type, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
